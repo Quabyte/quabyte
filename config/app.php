75b4aaf9b847f829f8cc43951e8366e20667fc8d
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2017-08-25 13:50:29 +0300

    Added Passport

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        Laravel\Passport\PassportServiceProvider::class,
         Barryvdh\Debugbar\ServiceProvider::class,
 
         /*
